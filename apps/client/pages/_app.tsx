b751126e40a1e674abd49cd2cff867a8bde3eeed
joonsung.um <joonsung.um@gmail.com>
2021-10-07 16:52:34 +0900

    move css baseline

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,6 +1,14 @@
+import React, { Fragment } from 'react'
 import type { AppProps } from 'next/app'
+import CssBaseline from '@mui/material/CssBaseline'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Fragment>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </Fragment>
+  )
 }
 export default MyApp
