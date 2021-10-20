f767241fb19bc78d14af2a46dc4f6ce28aaf6b5b
joonsung.um <joonsung.um@gmail.com>
2021-10-20 13:29:41 +0900

    update server status text

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,22 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!'
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const revision = require('child_process')
+      .execSync('git rev-parse --short HEAD')
+      .toString()
+      .trim()
+
+    const result = JSON.stringify(
+      {
+        name: 'api.namhae.life',
+        hash: revision,
+        uptime: process.uptime().toFixed(2),
+      },
+      null,
+      2,
+    )
+
+    return result
   }
 }
